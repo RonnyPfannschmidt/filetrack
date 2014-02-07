8d9c81a9bfc15275a323fc73cc8653fa5a63cf26
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-02-07 16:07:50 +0100

    initial import

--- filetrack.py
+++ filetrack.py
@@ -2,23 +2,30 @@ import sys
 import py
 import sqlite3
 
-IMPORT= "insert or ignore into filename_blob (filename, checksum) values (?, ?)"
+IMPORT= "insert or ignore into filename_blob (name, blob) values (?, ?)"
 
 def do_migrate(dbname):
-    from micromigrate import find_in_path, apply_ migrations
+    from micromigrate import find_in_path, apply_migrations
     here = py.path.local(__file__).dirpath().join('migrations')
     migrations = find_in_path(here)
+    from micromigrate.backend_script import ScriptBackend
     migrator = ScriptBackend(dbname)
     apply_migrations(migrator, migrations)
 
 
-
-def import(dbname, filename):
+def load(dbname, filename):
     do_migrate(dbname)
-    with open(f) as fp:
-       items = [line.split('  ', 1) for line in fp]
+    with open(filename) as fp:
+       items = [line.decode('utf-8', 'replace').split('  ', 1) for line in fp]
     db = sqlite3.connect(dbname);
     db.executemany(IMPORT, items)
+    db.commit()
+    db.close()
 
 
-if 
+
+if __name__ == '__main__':
+    args = sys.argv[2:]
+    command = sys.argv[1]
+    globals()[command](*args)
+
--- migrations/1_initial.sql
+++ migrations/1_initial.sql
@@ -1,4 +1,5 @@
--- name initial
+-- migration initial
+
 -- just a list of files as simple table
 
 create table filename_blob(name, blob);
